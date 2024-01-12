7237b51b5e5c0041583650e7273b5fe08cc93c28
nearinj <jnearing@hutlab11.rc.fas.harvard.edu>
2024-01-11 20:45:40 -0500

    Added all steps except Figure1B

--- run.py
+++ run.py
@@ -46,12 +46,37 @@ V1V2_db="input/SILVA_V1V2"
 V4V5_db="input/SILVA_V4V5"
 V1V2_assignments="output/V1V2_syn/"
 V4V5_assignments="output/V4V5_syn/"
+V1V2_para_taxa="output/V1V2_syn/taxonomic_assignments.tsv"
+V4V5_para_taxa="output/V4V5_syn/taxonomic_assignments.tsv"
+V1V2_bench="output/Figures/synth_mult_arc/V1V2_full_comparisons.RData"
+
+V4V5_mock_fasta="input/Mock_data/SRR3225703.fasta"
+V4V5_mock_out="output/V4V5_Mock"
+V4V5_mock_assignment="output/V4V5_Mock/taxonomic_assignments.tsv"
+V1V2_mock_fasta_uni="input/Mock_data/SRR3225701.unique.fasta"
+V1V2_mock_out="output/V1V2_Mock"
+V1V2_mock_assignment="output/V4V5_Mock/taxonomic_assignments.tsv"
+V1V2_mock_fasta="input/Mock_data/SRR3225701.fasta"
+V1V2_mock_counts="input/Mock_data/SRR3225701.count_table"
+Fig2="output/Figures/Fig_2A_MockHeatmap.png"
 
 
+ASD_reads="input/ASD_data/amplicons_fromKelsey.fasta"
+ASD_out="output/V4V5_ASD"
+ASD_assignments="output/V4V5_ASD/taxonomic_assignments.tsv"
+ASD_counts="input/ASD_data/all_samples_taxonomy_closed_reference.tsv"
+ASD_countSeq="input/ASD_data/all_samples_taxonomy_closed_reference_withseqs.tsv"
+ASD_figure="output/BrayCurtisPCoA.png"
 
 oligos_v4v5=os.path.join(args.paraDir, "input/primers/V4V5.oligos")
 oligos_v1v2=os.path.join(args.paraDir, "input/primers/V1V2.oligos")
 # Write workflow to download files
+
+workflow.add_task(
+    "gunzip input/Mock_data/SRR3225703.fasta.gz",
+    targets=V4V5_mock_fasta,
+    names="unzipping mock V4V5 fasta"
+)
 
 ## downlaod silva files
 workflow.add_task(
@@ -157,23 +182,84 @@ workflow.add_task(
 #run parathaa on V1V2
 
 workflow.add_task(
-    "parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [depends[1]] --output [targets[0]] --threads [args[0]]",
+    "parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [depends[1]] --output [args[1]] --threads [args[0]]",
     depends=[V1V2_db, V1V2_syn_reads],
-    targets=V1V2_assignments,
-    args=args.threads,
+    targets=V1V2_para_taxa,
+    args=[args.threads,V1V2_assignments],
     name="Assigning taxonomy to V1V2 synthetic reads"
 )
 
 #run parathaa on V4V5
 
 workflow.add_task(
-    "parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [depends[1]] --output [targets[0]] --threads [args[0]]",
+    "parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [depends[1]] --output [args[1]] --threads [args[0]]",
     depends=[V4V5_db, V4V5_syn_reads],
-    targets=V4V5_assignments,
-    args=args.threads,
+    targets=V4V5_para_taxa,
+    args=[args.threads, V4V5_assignments],
     name="Assigning taxonomy to V4V5 synthetic reads"
 )
 
+#run script to benchmark synthetic V1V2 and V4V5 data
 
-# Run the workflow
+workflow.add_task(
+    "Rscript src/Plots.Table.1.R -p [args[0]] --dada_db [depends[0]] --dada_db_sp [depends[1]] --paraAssignV4V5 [depends[2]] --paraAssignV1V2 [depends[3]] --queryV4V5 [depends[4]] --queryV1V2 [depends[5]] -t [depends[6]] -o [args[1]]",
+    depends=[dada2_seed_db, dada2_seed_db_sp, V4V5_para_taxa, V1V2_para_taxa, V4V5_syn_reads, V1V2_syn_reads, silva_taxonomy_file],
+    args=[args.paraDir, args.output],
+    targets=V1V2_bench,
+    name="Benchmarking synthetic assignments"
+
+)
+
+# Run parathaa on mock V4V5
+workflow.add_task(
+    "mkdir output/V4V5_Mock; parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [args[0]] --output [args[1]] --threads [args[2]]",
+    depends=[V4V5_db],
+    args=[V4V5_mock_fasta, V4V5_mock_out, args.threads],
+    targets=V4V5_mock_assignment,
+    name="Assigning parathaa taxonomy to V4V5 mock reads"
+)
+
+# Run parathaa on mock V1V2
+
+workflow.add_task(
+    "mkdir output/V1V2_Mock; parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [args[0]] --output [args[1]] --threads [args[2]]",
+    depends=[V1V2_db],
+    args=[V1V2_mock_fasta_uni, V1V2_mock_out, args.threads],
+    targets=V1V2_mock_assignment,
+    name="Assigning parathaa taxonomy to V1V2 mock reads"
+)
+
+# Run script to generate mock figures
+
+workflow.add_task(
+    "Rscript src/Plots.Figure.2.R --dada_db [depends[0]] --dada_db_sp [depends[1]] -o [args[0]] --paraAssignV4V5 [depends[2]] --paraAssignV1V2 [depends[3]] --fastaV4V5 [args[1]] --fastaV1V2 [args[2]] --fastaV1V2uni [args[3]] --V1V2Counts [args[4]]",
+    depends=[dada2_seed_db, dada2_seed_db_sp, V4V5_mock_assignment, V1V2_mock_assignment],
+    args=[args.output, V4V5_mock_fasta, V1V2_mock_fasta, V1V2_mock_fasta_uni, V1V2_mock_counts],
+    targets=Fig2,
+    name="Generate mock data figure (figure2)"
+)
+
+# Run parathaa on ASD data
+
+workflow.add_task(
+    "mkdir output/V4V5_ASD; parathaa_run_taxa_assignment --treeFiles [depends[0]] --query [args[0]] --output [args[1]] --threads [args[2]]",
+    depends=[V4V5_db],
+    args=[ASD_reads, ASD_out, args.threads],
+    targets=ASD_assignments,
+    name="Generating parathaa assignments for ASD dataset"
+)
+
+
+# Run script to generate ASD figures
+
+workflow.add_task(
+    "Rscript src/Plots.Figure.3.R --counts [args[0]] --countSeq [args[1]] --fasta [args[2]] --dada_db [depends[0]] --dada_db_sp [depends[1]] --paraAssign [depends[2]] -o [args[3]]",
+    depends=[dada2_seed_db, dada2_seed_db_sp, ASD_assignments],
+    args=[ASD_counts, ASD_countSeq, ASD_reads, args.output],
+    targets=ASD_figure,
+    name="Generating ASD Figures"
+)
+
+#done
+
 workflow.go()
