e56d2acf2d74a4d32fc845dbc86cac5de7f9958c
Jacob T. Nearing <nearing@broadinstitute.org>
2025-06-16 14:13:01 -0400

    removed old scripts no longer used in revised manuscript

--- src/Plot_Benchmarks_final.R
+++ src/Plot_Benchmarks_final.R
@@ -14,9 +14,9 @@ Options:
 
 opts <- docopt(doc)
 
-#opts$specific <- "~/Repos/Hills_Project/Parathaa_Project/Main_results_July_22/output_specific/benchmarks/"
-#opts$sensitive <- "~/Repos/Hills_Project/Parathaa_Project/Main_results_July_22/output/benchmarks/"
-
+opts$specific <- "~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/Main_results_July_22/output_specific/benchmarks/"
+opts$sensitive <- "~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/Main_results_July_22/output/benchmarks/"
+opts$output <- "~/Desktop"
 library(dplyr)
 library(ggplot2)
 library(reshape2)
@@ -228,18 +228,233 @@ mult_adjusted_Species <- get_data(opts$specific, level="Species", adjust_name="a
 nomult_adjusted_Species <- get_data(opts$specific, level="Species", adjust_name = "adjust", mult="nomult")
 sensitive_Species <- get_data(opts$sensitive, level="Species", adjust_name = "adjust", mult="mult")
 
+
+
+
 species_plots <- plot_benchs_species(mult_bench = mult_adjusted_Species,
                                      no_mult_bench = nomult_adjusted_Species,
                                      sens_bench = sensitive_Species)
+
+
+
 
 library(grid)
 gt = ggplot_gtable(ggplot_build(species_plots[[2]]))
 gt$heights[15] = 3*gt$heights[15]
 grid.draw(gt)
 
-species_plots[[1]]
-
 ggsave(filename = file.path(opts$output, "Species_full_data.pdf"), plot=species_plots[[1]], height = 8.25, width=11, units = "in")
 ggsave(filename = file.path(opts$output, "Species_filt_data.pdf"), plot=species_plots[[2]], height = 6, width=8, units = "in")
 
 
+##### Revisions
+
+
+species_plots[[2]]$data$variable <- as.character(species_plots[[2]]$data$variable)
+
+## change names
+
+species_plots[[2]]$data$variable[which(species_plots[[2]]$data$variable=="DADA2 Multi")] <- "Naive Bayes Multi-EM"
+species_plots[[2]]$data$variable[which(species_plots[[2]]$data$variable=="DADA2 Exact Match")] <- "Naive Bayes Multi-EM"
+species_plots[[2]]$data$variable[which(species_plots[[2]]$data$variable=="DADA2 Naive Bayes")] <- "Naive Bayes"
+species_plots[[2]]$data$variable[which(species_plots[[2]]$data$variable=="DADA2")] <- "Naive Bayes-EM"
+
+
+species_plots[[1]]$data$variable <- as.character(species_plots[[1]]$data$variable)
+species_plots[[1]]$data$variable[which(species_plots[[1]]$data$variable=="DADA2 Multi")] <- "Naive Bayes Multi-EM"
+species_plots[[1]]$data$variable[which(species_plots[[1]]$data$variable=="DADA2 Exact Match")] <- "Naive Bayes Multi-EM"
+species_plots[[1]]$data$variable[which(species_plots[[1]]$data$variable=="DADA2 Naive Bayes")] <- "Naive Bayes"
+species_plots[[1]]$data$variable[which(species_plots[[1]]$data$variable=="DADA2")] <- "Naive Bayes-EM"
+
+
+plot_data <- species_plots[[2]]$data
+
+
+### add in TaxaID data
+FL_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/holdout1/FL_Species_performance_adjust.tsv", header=T)
+
+FL_id_taxa <- FL_id_taxa %>% melt()
+colnames(FL_id_taxa)[1] <- c("Metric")
+FL_id_taxa$Region <- "Full"
+FL_id_taxa$variable <- "IDTAXA"
+colnames(FL_id_taxa)
+FL_id_taxa$Dataset <- "Holdout"
+FL_id_taxa1 <- FL_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+FL_id_taxa1_full <- FL_id_taxa
+
+FL_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/IDTaxa/holdout2/FL_Species_performance_adjust.tsv", header=T)
+
+FL_id_taxa <- FL_id_taxa %>% melt()
+colnames(FL_id_taxa)[1] <- c("Metric")
+FL_id_taxa$Region <- "Full"
+FL_id_taxa$variable <- "IDTAXA"
+colnames(FL_id_taxa)
+FL_id_taxa$Dataset <- "Holdout"
+FL_id_taxa2 <- FL_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+FL_id_taxa2_full <- FL_id_taxa
+
+FL_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/IDTaxa/holdout3//FL_Species_performance_adjust.tsv", header=T)
+
+FL_id_taxa <- FL_id_taxa %>% melt()
+colnames(FL_id_taxa)[1] <- c("Metric")
+FL_id_taxa$Region <- "Full"
+FL_id_taxa$variable <- "IDTAXA"
+colnames(FL_id_taxa)
+FL_id_taxa$Dataset <- "Holdout"
+FL_id_taxa3 <- FL_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+FL_id_taxa3_full <- FL_id_taxa
+
+FL_id_taxa <- rbind(FL_id_taxa1, FL_id_taxa2, FL_id_taxa3)
+
+## load in V1V2
+V1V2_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/holdout1/V1V2_Species_performance_adjust.tsv", header=T)
+
+V1V2_id_taxa <- V1V2_id_taxa %>% melt()
+colnames(V1V2_id_taxa)[1] <- c("Metric")
+V1V2_id_taxa$Region <- "V1V2"
+V1V2_id_taxa$variable <- "IDTAXA"
+colnames(V1V2_id_taxa)
+V1V2_id_taxa$Dataset <- "Holdout"
+V1V2_id_taxa1 <- V1V2_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+V1V2_id_taxa1_full <- V1V2_id_taxa
+
+
+V1V2_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/IDTaxa/holdout2//V1V2_Species_performance_adjust.tsv", header=T)
+
+V1V2_id_taxa <- V1V2_id_taxa %>% melt()
+colnames(V1V2_id_taxa)[1] <- c("Metric")
+V1V2_id_taxa$Region <- "V1V2"
+V1V2_id_taxa$variable <- "IDTAXA"
+colnames(V1V2_id_taxa)
+V1V2_id_taxa$Dataset <- "Holdout"
+V1V2_id_taxa2 <- V1V2_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+V1V2_id_taxa2_full <- V1V2_id_taxa
+
+V1V2_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/IDTaxa/holdout3//V1V2_Species_performance_adjust.tsv", header=T)
+
+V1V2_id_taxa <- V1V2_id_taxa %>% melt()
+colnames(V1V2_id_taxa)[1] <- c("Metric")
+V1V2_id_taxa$Region <- "V1V2"
+V1V2_id_taxa$variable <- "IDTAXA"
+colnames(V1V2_id_taxa)
+V1V2_id_taxa$Dataset <- "Holdout"
+V1V2_id_taxa3 <- V1V2_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+V1V2_id_taxa3_full <- V1V2_id_taxa
+
+V1V2_id_taxa <- rbind(V1V2_id_taxa1, V1V2_id_taxa2, V1V2_id_taxa3)
+
+## Load in V4V5
+
+V4V5_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/holdout1/V4V5_Species_performance_adjust.tsv", header=T)
+V4V5_id_taxa <- V4V5_id_taxa %>% melt()
+colnames(V4V5_id_taxa)[1] <- c("Metric")
+V4V5_id_taxa$Region <- "V4V5"
+V4V5_id_taxa$variable <- "IDTAXA"
+colnames(V4V5_id_taxa)
+V4V5_id_taxa$Dataset <- "Holdout"
+V4V5_id_taxa1 <- V4V5_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+V4V5_id_taxa1_full <- V4V5_id_taxa
+
+V4V5_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/IDTaxa/holdout2//V4V5_Species_performance_adjust.tsv", header=T)
+V4V5_id_taxa <- V4V5_id_taxa %>% melt()
+colnames(V4V5_id_taxa)[1] <- c("Metric")
+V4V5_id_taxa$Region <- "V4V5"
+V4V5_id_taxa$variable <- "IDTAXA"
+colnames(V4V5_id_taxa)
+V4V5_id_taxa$Dataset <- "Holdout"
+V4V5_id_taxa2 <- V4V5_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+V4V5_id_taxa2_full <- V4V5_id_taxa
+
+V4V5_id_taxa <- read.table("~/Dropbox_Harvard/hutlab/Jacob/Repos/Hills_Project/Parathaa_Project/IDTaxa_testing/FINAL_RESULTS/IDTaxa/holdout3//V4V5_Species_performance_adjust.tsv", header=T)
+V4V5_id_taxa <- V4V5_id_taxa %>% melt()
+colnames(V4V5_id_taxa)[1] <- c("Metric")
+V4V5_id_taxa$Region <- "V4V5"
+V4V5_id_taxa$variable <- "IDTAXA"
+colnames(V4V5_id_taxa)
+V4V5_id_taxa$Dataset <- "Holdout"
+V4V5_id_taxa3 <- V4V5_id_taxa %>% filter(!Metric %in% c("Unassigned Correct", "Unassigned Incorrect", "Accuracy"))
+V4V5_id_taxa3_full <- V4V5_id_taxa
+
+V4V5_id_taxa <- rbind(V4V5_id_taxa1, V4V5_id_taxa2, V4V5_id_taxa3)
+
+plot_data <- rbind(plot_data, FL_id_taxa, V4V5_id_taxa, V1V2_id_taxa)
+
+cols <- c("Parathaa Specific (default)"="#FA8072",
+          "Parathaa Sensitive"="#660000",
+          "Naive Bayes-EM"="#143d59",
+          "Naive Bayes Multi-EM"="#1e1eff",
+          "Naive Bayes"="#47008e",
+          "IDTAXA"="#BA8E23")
+
+shapes <- c("Parathaa Specific (default)"=16,
+            "Parathaa Sensitive"=16,
+            "Naive Bayes-EM"=17,
+            "Naive Bayes"=17,
+            "Naive Bayes Multi-EM"=17,
+            "IDTAXA"=18)
+
+
+small_plot <- ggplot(plot_data, aes(x=Dataset, y=as.numeric(value), group=variable, color=variable, shape=variable)) + 
+  geom_jitter(position=position_jitterdodge(dodge.width = 1, jitter.width = .1), size=3, alpha=0.6) +
+  #geom_point(stat="summary", fun="mean", shape="_", position = position_dodge(width = 1), size=10) +
+  facet_grid(rows=vars(Metric), cols=vars(Region), scales="free_y") +
+  theme_bw() + 
+  scale_color_manual(values = cols, name="Software", breaks=
+                       c("Parathaa Specific (default)", "Parathaa Sensitive", "Naive Bayes-EM", "Naive Bayes Multi-EM", 
+                        "Naive Bayes", "IDTAXA"))+ 
+  ylab("") +
+  ggtitle("Species") + theme(strip.text.y=element_text(angle=0), axis.text.x = element_text(angle=65, vjust = 0.5)) +
+  scale_shape_manual(values=shapes, name="Software", breaks=
+                       c("Parathaa Specific (default)", "Parathaa Sensitive", "DADA2", "DADA2 Multi", 
+                         "DADA2 Exact Match", "DADA2 Naive Bayes", "IDTAXA"))+
+  scale_y_continuous(breaks=scales::pretty_breaks(n=3)) +
+  theme(panel.spacing.y = unit(.5, "lines")) +
+  xlab("") +
+  theme(axis.text.x=element_blank(), axis.ticks.x =  element_blank())
+
+ggsave("~/Dropbox_Harvard/hutlab/Jacob/Paper_Submissions/Parathaa/NAR/Figures/Figure2/Figure2_revised_panelA.pdf",
+       width=12, height=6)
+
+
+### supplemental plot
+FL_id_taxa_full <- rbind(FL_id_taxa1_full, FL_id_taxa2_full, FL_id_taxa3_full)
+V1V2_id_taxa_full <- rbind(V1V2_id_taxa1_full, V1V2_id_taxa2_full, V1V2_id_taxa3_full)
+V4V5_id_taxa_full <- rbind(V4V5_id_taxa1_full, V4V5_id_taxa2_full, V4V5_id_taxa3_full)
+
+plot_dada2 <- rbind(species_plots[[1]]$data, FL_id_taxa_full, V1V2_id_taxa_full, V4V5_id_taxa_full)
+
+
+
+full_plot <- ggplot(plot_dada2, aes(x=Dataset, y=as.numeric(value), group=variable, color=variable, shape=variable)) + 
+  #geom_jitter(position=position_jitterdodge(dodge.width = 1, jitter.width = .1), size=3, alpha=0.7) +
+  geom_point(stat="summary", fun="mean", position = position_dodge(width = 1), size=2) +
+  facet_grid(rows=vars(Metric), cols=vars(Region), scales="free_y") +
+  theme_bw() + 
+  scale_color_manual(values = cols, name="Software", breaks=
+                       c("Parathaa Specific (default)", "Parathaa Sensitive", "Naive Bayes-EM", "Naive Bayes Multi-EM", 
+                         "Naive Bayes", "IDTAXA"))+ 
+  ylab("") +
+  ggtitle("Species") + theme(strip.text.y=element_text(angle=0), axis.text.x = element_text(angle=65, vjust = 0.5)) +
+  scale_y_continuous(breaks=scales::pretty_breaks(n=3)) +
+  scale_shape_manual(name="Software", values=shapes, breaks=
+                       c("Parathaa Specific (default)", "Parathaa Sensitive", "DADA2", "DADA2 Multi", 
+                         "DADA2 Exact Match", "DADA2 Naive Bayes")) +
+  geom_rect(xmin=0,
+            xmax=1.5,
+            ymin=0, ymax=1, color=NA, fill="grey", alpha=0.001) +
+  geom_rect(xmin=1.5,
+            xmax=2.5,
+            ymin=0, ymax=1, color=NA, fill="black", alpha=0.01) +
+  geom_rect(xmin=2.5,
+            xmax=3.5,
+            ymin=0, ymax=1, color=NA, fill="grey", alpha=0.001) +
+  geom_rect(xmin=3.5,
+            xmax=4.5,
+            ymin=0, ymax=1, color=NA, fill="black", alpha=0.01) +
+  geom_rect(xmin=4.5,
+            xmax=6,
+            ymin=0, ymax=1, color=NA, fill="grey", alpha=0.001)
+full_plot
+
+ggsave("~/Dropbox_Harvard/hutlab/Jacob/Paper_Submissions/Parathaa/NAR/Figures/Figure2/supplemental_bench.pdf",
+       width=14, height=8)
